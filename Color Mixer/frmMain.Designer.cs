e44cf59b0d1541f74512a9f55b9bd2dd2a7215ce
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-19 23:00:24 -0600

    cs 1030 c#

---
+++ Color Mixer/frmMain.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Color_Mixer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlColor = new System.Windows.Forms.Panel();
+            this.hsbRed = new System.Windows.Forms.HScrollBar();
+            this.hsbGreen = new System.Windows.Forms.HScrollBar();
+            this.hsbBlue = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnlColor
+            // 
+            this.pnlColor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlColor.Location = new System.Drawing.Point(23, 12);
+            this.pnlColor.Name = "pnlColor";
+            this.pnlColor.Size = new System.Drawing.Size(281, 100);
+            this.pnlColor.TabIndex = 0;
+            // 
+            // hsbRed
+            // 
+            this.hsbRed.LargeChange = 25;
+            this.hsbRed.Location = new System.Drawing.Point(34, 128);
+            this.hsbRed.Maximum = 279;
+            this.hsbRed.Name = "hsbRed";
+            this.hsbRed.Size = new System.Drawing.Size(170, 17);
+            this.hsbRed.TabIndex = 1;
+            this.hsbRed.Value = 128;
+            this.hsbRed.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hsbRed_Scroll);
+            // 
+            // hsbGreen
+            // 
+            this.hsbGreen.LargeChange = 25;
+            this.hsbGreen.Location = new System.Drawing.Point(34, 162);
+            this.hsbGreen.Maximum = 279;
+            this.hsbGreen.Name = "hsbGreen";
+            this.hsbGreen.Size = new System.Drawing.Size(170, 17);
+            this.hsbGreen.TabIndex = 2;
+            this.hsbGreen.Value = 128;
+            this.hsbGreen.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hsbGreen_Scroll);
+            // 
+            // hsbBlue
+            // 
+            this.hsbBlue.LargeChange = 25;
+            this.hsbBlue.Location = new System.Drawing.Point(34, 195);
+            this.hsbBlue.Maximum = 279;
+            this.hsbBlue.Name = "hsbBlue";
+            this.hsbBlue.Size = new System.Drawing.Size(170, 17);
+            this.hsbBlue.TabIndex = 3;
+            this.hsbBlue.Value = 128;
+            this.hsbBlue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hsbBlue_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Blue";
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed.Location = new System.Drawing.Point(263, 132);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(25, 13);
+            this.lblRed.TabIndex = 7;
+            this.lblRed.Text = "128";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreen.Location = new System.Drawing.Point(263, 166);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(25, 13);
+            this.lblGreen.TabIndex = 8;
+            this.lblGreen.Text = "128";
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlue.Location = new System.Drawing.Point(263, 199);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(25, 13);
+            this.lblBlue.TabIndex = 9;
+            this.lblBlue.Text = "128";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(60, 226);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(206, 226);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 261);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hsbBlue);
+            this.Controls.Add(this.hsbGreen);
+            this.Controls.Add(this.hsbRed);
+            this.Controls.Add(this.pnlColor);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmMain";
+            this.Text = "Color Mixer";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlColor;
+        private System.Windows.Forms.HScrollBar hsbRed;
+        private System.Windows.Forms.HScrollBar hsbGreen;
+        private System.Windows.Forms.HScrollBar hsbBlue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Color Mixer/frmMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_Mixer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnReset_Click(sender, e);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblRed.Text = "128";
+            hsbRed.Value = 128;
+            lblGreen.Text = "128";
+            hsbGreen.Value = 128;
+            lblBlue.Text = "128";
+            hsbBlue.Value = 128;
+            DisplayColor();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void hsbRed_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblRed.Text = hsbRed.Value.ToString();
+            DisplayColor();
+        }
+
+        private void hsbGreen_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblGreen.Text = hsbGreen.Value.ToString();
+            DisplayColor();
+        }
+
+        private void hsbBlue_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblBlue.Text = hsbBlue.Value.ToString();
+            DisplayColor();
+        }
+        private void DisplayColor()
+        {
+            pnlColor.BackColor = Color.FromArgb(hsbRed.Value, hsbGreen.Value, hsbBlue.Value);
+        }
+    }
+}
